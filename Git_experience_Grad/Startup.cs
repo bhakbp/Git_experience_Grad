9ba0d7ddb9b613dd31595a8a1c6c4158e075bdbb
bhakbp <kammala.bhanu@gmail.com>
2024-06-09 12:28:18 +0530

    modified from hw to hwt

--- Git_experience_Grad/Startup.cs
+++ Git_experience_Grad/Startup.cs
@@ -32,7 +32,7 @@ namespace Git_experience_Grad
             {
                 endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync("Hello World!");
+                    await context.Response.WriteAsync("Hello World! how r u");
                 });
             });
         }
